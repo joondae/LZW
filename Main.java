1cd4a553bdc4d909ec5fc709f549e4f510f91f91
joondae <68300257+joondae@users.noreply.github.com>
2020-09-09 12:05:05 -0700

    Update Main.java
    Main now tests Theiss's .txt files

--- Main.java
+++ Main.java
@@ -6,7 +6,26 @@ public class Main {
 		ArrayList<Integer> codes = new ArrayList<Integer>(512);
 		Decoder d = new Decoder();
 		
+		/*
 		codes = e.encodeFile("fileToEncode.txt");
+		System.out.println(codes);
 		d.decodeFile(codes);
+		*/
+		
+		codes = e.encodeFile("lzw-file 1");
+		System.out.println(codes);
+		//d.decodeFile(codes);
+		
+		/*
+		codes = e.encodeFile("lzw-file 2.txt");
+		System.out.println(codes);
+		d.decodeFile(codes);
+		*/
+		
+		/*
+		codes = e.encodeFile("lzw-file 3.txt");
+		System.out.println(codes);
+		d.decodeFile(codes);
+		*/
 	}
 }
