2235c7c641a70eee789ba7725d14700f27ecd571
joondae <68300257+joondae@users.noreply.github.com>
2020-09-11 03:02:41 -0700

    fix: (hopefully) functioning basic LZW encoding algorithm

--- Encoder.java
+++ Encoder.java
@@ -7,62 +7,55 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-//HUB DESCRIPTION:
-//created repo first, then changed project properties: properties>project nature>add>java
-	//is this dangerous?
-	//(rather than creating java project first then creating repo w/ java project selected as repo folder)
-
-//NOTE: Your table of codes should have a MAXIMUM size,
-//after which you no longer add any more codes to the table;
-//what size that is is up to you.
-
-//How you output the codes into the encoded file: also up to you.
-//What you initialize the table of codes with: up to you.
-//What data structures you use: up to you.
-//Lots of things: up to you!
-
 public class Encoder {
 	public ArrayList<Integer> encodeFile(String fileName) {
-		//not sure what we're supposed to do after making the codes...
-		return generateCodes(fileName, initializeDictionary());
+		return generateCodestream(fileName, initializeDictionary());
 	}
 
 	private HashMap<String, Integer> initializeDictionary() {
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		//512 for 9 bit encoding
+		HashMap<String, Integer> dictionary = new HashMap<String, Integer>(512);
 		
 		//adds ASCII table (all characters w/ decimal values from 0-255)
 		for(int i = 0; i < 256; i++) {
-			//alternative code for key: (char) i + ""
-			map.put(Character.toString((char) i), i);
+			dictionary.put(Character.toString((char) i), i);
 		}
 		
-		return map;
+		return dictionary;
 	}
 	
-	private ArrayList<Integer> generateCodes(String fileToEncode, HashMap<String, Integer> map) {
-		//512 for 9 bit encoding
-		ArrayList<Integer> codes = new ArrayList<Integer>(512);
+	private ArrayList<Integer> generateCodestream(String fileToEncode, HashMap<String, Integer> dictionary) {
+		ArrayList<Integer> codestream = new ArrayList<Integer>();
+		String p = "";
+		//change to 257 for EOF marker
+		int dictionarySize = 256;
 		
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(new File(fileToEncode)));
 			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("encodedFile.txt")));
 			
 			while(br.ready()) {
-				//String because p can be multiple letters
-				String p = null;
-				char c = '\0';
+				char c = (char) br.read();
 				
-				if(map.containsKey(p + c)) {
+				//if present in dictionary:
+				if(dictionary.containsKey(p + c)) {
 					//P = concat(P,C)
 					p += c;
+					
+					//to cover last read
+					if(br.ready() == false) {
+						codestream.add(dictionary.get(p));
+						bw.write(dictionary.get(p));
+					}
 				}
+				//if not present in dictionary:
 				else {
 					//output the code word which denotes P to the codestream
-					codes.add(Integer.parseInt(p));
-					bw.write(Integer.parseInt(p) + " ");
-					
+					codestream.add(dictionary.get(p));
+					bw.write(dictionary.get(p));
+
 					//add the string concat(P,C) to the dictionary
-					map.put(p + c, Integer.parseInt(p + c));
+					dictionary.put(p + c, dictionarySize++);
 					
 					//P = C
 					p = Character.toString(c);
@@ -75,6 +68,6 @@ public class Encoder {
 			e.printStackTrace();
 		}
 		
-		return codes;
+		return codestream;
 	}
 }
