92f60136c680361d8138faf818f08184ca5e3e8b
saculgrad <70218089+saculgrad@users.noreply.github.com>
2020-09-29 01:13:41 -0700

    DecoderFinalEdits
    Adding plenty of commenting and deleted all extraneous code

--- src/Decoder.java
+++ src/Decoder.java
@@ -3,112 +3,49 @@ import java.io.*;
 
 public class Decoder {
 		
-	public static HashMap<Integer, String> dictionary = new HashMap<Integer, String>(512);
+	public static HashMap<Integer, String> dictionary = new HashMap<Integer, String>(512);//Dictionary containing all relevant characters and Strings from the original text (N.B. This dictionary is Integer,String but the other was String,Integer)
 	
 	public void decodeFile(String encodedFile) {//method to decode file
 		try{
 
 			BufferedReader br=new BufferedReader(new FileReader(new File(encodedFile)));//reads the file of codes from the encoder
-			BufferedWriter bw=new BufferedWriter(new FileWriter(new File(encodedFile.substring(0,encodedFile.length()-8)+"-decoded.txt")));
+			BufferedWriter bw=new BufferedWriter(new FileWriter(new File(encodedFile.substring(0,encodedFile.length()-8)+"-decoded.txt"))); //outputs the text into a file by the name of [OriginalFile]-decoded.txt
 			ArrayList<Integer> codes=new ArrayList<Integer>();//arraylist for codes
 			
 			for(int i = 0; i < 256; i++) {
-				dictionary.put(i,Character.toString((char) i));
+				dictionary.put(i,""+(char)i); //adds a String containing each char of value 0 through 255 in the ascii table to the dictionary
 			}
 
-			while(br.ready()){
-				String currentString = "";
-				int currentChar = br.read();
+			while(br.ready()){ //Goes through each character in the input file until it reaches the end/null, puts each code in the input file into an integer arraylist
+				
+				String currentString = ""; //Keeps track of the current code in the file we are on, add the chars to this until you reach a space
+				int currentChar = br.read(); //Keeps track of the current char in the file we are on
 
-				while(currentChar!=-1&&currentChar!=(char)' '){
+				while(currentChar!=-1&&currentChar!=(char)' '){ //Goes through the file while current character is not a space or the end of the file
 					currentString += (char)currentChar;
 					currentChar = br.read();
 				}
-				codes.add(Integer.parseInt(currentString));
+
+				codes.add(Integer.parseInt(currentString)); //adds the current code to the dictionary
 
 			}
-			for (int i = 0; i < codes.size()-1; i++){
+
+			for (int i = 0; i < codes.size()-1; i++){ //Goes through each code except the last one (because I deal with two codes at the same time) and recreates the dictionary with those codes
+				
 				if (dictionary.containsKey(codes.get(i+1))){
-					dictionary.put(256+i,""+dictionary.get(codes.get(i))+dictionary.get(codes.get(i+1)).charAt(0));
-				}else{
-					dictionary.put(256+i,""+dictionary.get(codes.get(i))+dictionary.get(codes.get(i)).charAt(0));
+					dictionary.put(256+i,""+dictionary.get(codes.get(i))+dictionary.get(codes.get(i+1)).charAt(0)); //Add the dictionary String of the first code + the dictionary String of the second code as the next entry in the dictionary
+				}else{//handles the special case when there are 3+ consecutive of the same char in the original text [Ex: 97 256 --> aaa]
+					dictionary.put(256+i,""+dictionary.get(codes.get(i))+dictionary.get(codes.get(i)).charAt(0)); //Handles the special case mentioned above
 				}
 			}
 			
-			for (Integer codeValue : codes){
-				bw.write(dictionary.get(codeValue));
+			for (Integer codeValue : codes){ //Goes through each code in the input file
+				bw.write(dictionary.get(codeValue)); //Outputs the dictionary String of that code
 			}
+
 			br.close();
 			bw.close();
 
-
-
-			/**
-
-
-				//knowing when the z will pop up so we can begin
-				
-				
-				//while br reads a character
-				while ( ( a = br.read () ) != -1 ){
-					//now we begin
-					if(begin==true){
-						//if this string needs to be added to dictionary
-						if(newString==true){
-							//keep adding chars until we hit the sizeLimit of the string
-							add+=(char)a;
-							sizeLimit+=1;
-							if(sizeLimit==size){
-								//add string and index to dictionary, then reset.
-								dictionary.put(add, currentCombination);
-								sizeLimit=0;
-								newString=false;
-								add="";
-							}
-						}else {
-							//we finished the index of the string we want
-							if(String.valueOf((char)a).equals("$")){
-								currentCombination=Integer.parseInt(add);
-								add="";
-							}
-							//we got the length of the string we want
-							else if(String.valueOf((char)a).equals("#")){
-								size=Integer.parseInt(add);
-								newString=true;
-							}
-							//adding chars to the string
-							else{
-								add+=((char)a);
-							}
-						}
-					}
-				}
-			}
-			int temp;//temp int for storing decoded values
-			int c=0;//int to store the parse of nums
-			
-			String nums = null;//string of decoded value
-			
-			while ((temp=br.read())!=-1){
-				if (temp==32){
-					codes.add(c=Integer.parseInt(nums));
-					nums="";		
-				}else{
-					nums+=(char)temp;
-				}
-			}
-			codes.add(c=Integer.parseInt(nums));
-			br.close();
-			String output="";
-			//adds codes into output
-			for (int i=0;i<codes.size();i++){
-				output+=dictionary.get(codes.get(i));
-			}
-			//creates file and adds the output to the file
-			bw.append(output);
-			bw.close();
-			}
-			*/
 		}catch(Exception exe){
 			exe.printStackTrace();
 		}
